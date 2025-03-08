82d29a7808368ca6bd74dc1a6d3b9762d4ba2bd8
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-08 11:58:40 +0500

    Authentication

--- src/components/Loaders.tsx
+++ src/components/Loaders.tsx
@@ -6,7 +6,7 @@ const Loaders: React.FC<LoaderPropsTypes> = ({ className }) => {
   return (
     <div className="flex justify-center items-center ">
       <div
-        className={`  animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent rounded-full`}
+        className={` ${className} animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent rounded-full`}
         role="status"
         aria-label="loading"
       ></div>

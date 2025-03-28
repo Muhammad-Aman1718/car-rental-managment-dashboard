5fb8c3b8b1e32cc6b1667ba3402a8b3a10b350ac
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-28 23:06:43 +0500

    working on data

--- src/app/api/userData/route.ts
+++ src/app/api/userData/route.ts
@@ -2,60 +2,59 @@ import { prisma } from "@/config/prisma";
 import authOptions from "@/lib/auth";
 import { AxiosError } from "axios";
 import { getServerSession } from "next-auth/next";
- import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
-  try {
-    const session = await getServerSession(authOptions);
-    console.log("this is session ======>", session);
+// export const GET = async () => {
+//   try {
+//     const session = await getServerSession(authOptions);
+//     console.log("this is session ======>", session);
 
-    if (!session) {
-      return NextResponse.json({ session, authOptions });
-    }
+//     if (!session) {
+//       return NextResponse.json({ session, authOptions });
+//     }
 
-    const user = await prisma.user.findUnique({
-      where: { id: session?.user?.id },
-    });
+//     const user = await prisma.user.findUnique({
+//       where: { id: session?.user.id },
+//     });
 
-    console.log("this is user =====>", user );
+//     console.log("this is user =====>", user);
 
-    if (!user) {
-      return NextResponse.json({ status: 404, message: "User not found" });
-    }
-    return NextResponse.json({
-      status: 200,
-      message: "Successfully retrieved user info",
-      user,
-    });
-  } catch (error) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: error instanceof Error ? error.message : "An error occurred",
-      },
-      { status: 500 }
-    );
-  }
-};
+//     if (!user) {
+//       return NextResponse.json({ status: 404, message: "User not found" });
+//     }
+//     return NextResponse.json({
+//       status: 200,
+//       message: "Successfully retrieved user info",
+//       user,
+//     });
+//   } catch (error) {
+//     return NextResponse.json(
+//       {
+//         success: false,
+//         message: error instanceof Error ? error.message : "An error occurred",
+//       },
+//       { status: 500 }
+//     );
+//   }
+// };
 
 export const PUT = async (req: NextRequest, res: NextResponse) => {
   try {
     const body = await req.json();
+    // const { liveIn, streetAddress, dateOfBirth, gender } = body;
+    console.log("this is body : ", body);
 
-    if (
-      !body.liveIn ||
-      !body.streetAddress ||
-      !body.dateOfBirth ||
-      !body.gender
-    ) {
+    if (body.liveIn || body.streetAddress || body.dateOfBirth || body.gender) {
       return NextResponse.json(
         { success: false, message: "All fields are required" },
         { status: 400 }
       );
     }
 
+    const session = await getServerSession(authOptions);
+
     const userData = await prisma.user.update({
-      where: { id },
+      where: { id: session?.user?.id },
       data: {
         liveIn: body.liveIn,
         streetAddress: body.streetAddress,

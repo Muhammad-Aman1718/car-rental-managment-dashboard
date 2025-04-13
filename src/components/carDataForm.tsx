eeea771039062f868fd60b95f221fd02b4f6337c
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-04-13 12:25:16 +0500

    working

--- src/components/carDataForm.tsx
+++ src/components/carDataForm.tsx
@@ -1,17 +1,52 @@
 import React from "react";
 import CarDataFormInput from "./inputs/CarDataFormInput";
+import useDasboard from "@/hooks/useDasboard";
 
 interface CarDataFormPropsTypes {
   onClick?: () => void;
 }
 
 const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
+  const {
+    openForm,
+    setOpenForm,
+    carName,
+    setCarName,
+    fuelType,
+    setFuelType,
+    transmission,
+    setTransmission,
+    mileage,
+    setMileage,
+    topSpeed,
+    setTopSpeed,
+    price,
+    setPrice,
+    color,
+    setColor,
+    engineCapacity,
+    setEngineCapacity,
+    seatingCapacity,
+    setSeatingCapacity,
+    registrationNumber,
+    setRegistrationNumber,
+    carType,
+    setCarType,
+    modelYear,
+    setModelYear,
+    doors,
+    setDoors,
+    hasAC,
+    setHasAC,
+  } = useDasboard();
   return (
     <div className=" dark:bg-[#242731] overflow-y-auto mx-auto p-6 bg-white shadow-xl rounded-xl ">
       <h2 className="text-2xl font-bold mb-6 dark:text-white">Add New Car</h2>
       <form>
         <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-5  ">
           <CarDataFormInput
+            value={carName}
+            onChange={(e) => setCarName(e.target.value)}
             label="Car Name"
             type="text"
             name="name"
@@ -20,6 +55,8 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
             required
           />
           <CarDataFormInput
+            value={fuelType}
+            onChange={(e) => setFuelType(e.target.value)}
             label="Fuel Type"
             type="text"
             name="fuelType"
@@ -28,6 +65,8 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
             required
           />
           <CarDataFormInput
+            value={transmission}
+            onChange={(e) => setTransmission(e.target.value)}
             label="Transmission"
             type="text"
             name="transmission"
@@ -35,9 +74,11 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
             className="w-full border p-2 rounded"
             required
           />
-          <CarDataFormInput
+          {/* <CarDataFormInput
+            value={carName}
+            onChange={(e) => setCarName(e.target.value)}
             label="Mileage (km/l)"
-            type="number"
+            type="text"
             name="mileage"
             placeholder="Enter your Mileage (km/l)"
             className="w-full border p-2 rounded"
@@ -45,7 +86,7 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
           />
           <CarDataFormInput
             label="Top Speed (km/h)"
-            type="number"
+            type="text"
             name="topSpeed"
             placeholder="Enter your Top speed (km/h)"
             className="w-full border p-2 rounded"
@@ -53,7 +94,7 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
           />
           <CarDataFormInput
             label="Price ($)"
-            type="number"
+            type="text"
             name="price"
             placeholder="Enter your price ($)"
             className="w-full border p-2 rounded"
@@ -69,7 +110,7 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
           />
           <CarDataFormInput
             label="Engine Capacity (cc)"
-            type="number"
+            type="text"
             name="engineCapacity"
             placeholder="Enter your Engine Capacity (cc)"
             className="w-full border p-2 rounded"
@@ -77,7 +118,7 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
           />
           <CarDataFormInput
             label="Seating Capacity"
-            type="number"
+            type="text"
             name="seatingCapacity"
             placeholder="Enter your Seating Capacity"
             className="w-full border p-2 rounded"
@@ -101,7 +142,7 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
           />
           <CarDataFormInput
             label="Model Year"
-            type="number"
+            type="text"
             name="year"
             placeholder="Enter your Model Year"
             className="w-full border p-2 rounded"
@@ -109,7 +150,108 @@ const CarDataForm: React.FC<CarDataFormPropsTypes> = ({ onClick }) => {
           />
           <CarDataFormInput
             label="Doors"
-            type="number"
+            type="text"
+            name="doors"
+            placeholder="Enter number of Doors"
+            className="w-full border p-2 rounded"
+            required
+          /> */}
+
+          <CarDataFormInput
+            value={mileage}
+            onChange={(e) => setMileage(e.target.value)}
+            label="Mileage (km/l)"
+            type="text"
+            name="mileage"
+            placeholder="Enter your Mileage (km/l)"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={topSpeed}
+            onChange={(e) => setTopSpeed(e.target.value)}
+            label="Top Speed (km/h)"
+            type="text"
+            name="topSpeed"
+            placeholder="Enter your Top speed (km/h)"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            label="Price ($)"
+            type="text"
+            name="price"
+            placeholder="Enter your price ($)"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            label="Color"
+            type="text"
+            name="color"
+            placeholder="Enter your color"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={engineCapacity}
+            onChange={(e) => setEngineCapacity(e.target.value)}
+            label="Engine Capacity (cc)"
+            type="text"
+            name="engineCapacity"
+            placeholder="Enter your Engine Capacity (cc)"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={seatingCapacity}
+            onChange={(e) => setSeatingCapacity(e.target.value)}
+            label="Seating Capacity"
+            type="text"
+            name="seatingCapacity"
+            placeholder="Enter your Seating Capacity"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={registrationNumber}
+            onChange={(e) => setRegistrationNumber(e.target.value)}
+            label="Registration Number"
+            type="text"
+            name="registrationNo"
+            placeholder=" Enter your car registration number"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={carType}
+            onChange={(e) => setCarType(e.target.value)}
+            label="Car Type"
+            type="text"
+            name="carType"
+            placeholder="Enter your Car Type"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={modelYear}
+            onChange={(e) => setModelYear(e.target.value)}
+            label="Model Year"
+            type="text"
+            name="year"
+            placeholder="Enter your Model Year"
+            className="w-full border p-2 rounded"
+            required
+          />
+          <CarDataFormInput
+            value={doors}
+            onChange={(e) => setDoors(e.target.value)}
+            label="Doors"
+            type="text"
             name="doors"
             placeholder="Enter number of Doors"
             className="w-full border p-2 rounded"
--- src/components/inputs/CarDataFormInput.tsx
+++ src/components/inputs/CarDataFormInput.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 interface carDataFormInputPropsTypes {
+  value?: string;
   divStyle?: string;
   label?: string;
   type?: string;
   name?: string;
   placeholder?: string;
   className?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
 }
 
 const CarDataFormInput: React.FC<carDataFormInputPropsTypes> = ({
+  value,
   divStyle,
   label,
   type,
@@ -18,6 +21,7 @@ const CarDataFormInput: React.FC<carDataFormInputPropsTypes> = ({
   placeholder,
   className,
   required,
+  onChange,
 }) => {
   return (
     <div className={divStyle}>
@@ -28,12 +32,14 @@ const CarDataFormInput: React.FC<carDataFormInputPropsTypes> = ({
         {label}
       </label>
       <input
+        value={value}
         type={type}
         name={name}
         placeholder={placeholder}
         className={` ${className} outline-none w-full bg-white  placeholder:text-[14px] dark:text-white dark:placeholder:text-[#7C7C8D] dark:bg-[#242731] placeholder:text-[#5F6165] placeholder:leading-[18.23px] placeholder:font-medium 
  `}
         required={required}
+        onChange={onChange}
       />
     </div>
   );

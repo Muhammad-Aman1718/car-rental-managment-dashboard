c675277ab44826c56ee75bf21d0e1f804fd2dd39
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-04-20 12:21:57 +0500

    Completed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-module.exports = {
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
-};
+];
 
 export default eslintConfig;

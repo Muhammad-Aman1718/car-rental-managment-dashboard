ca30d62c6a9ff081da337aaf8865de8f4692163a
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-04-22 16:17:09 +0500

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,38 +9,42 @@ import FindVehicleInput from "@/components/inputs/FindVehicleInput";
 
 const Home = () => {
   return (
-    <div className=" bg-[#0F0F0F]">
-      <MainContainer className=" border text-white">
-        <nav className="flex items-center justify-between my-[22px] ">
-          <IoReorderTwoOutline className="w-5 h-5 " />
-          <Image src={HeadingLogo} alt="Heading does not show " />
-          <Link href={`/auth/signUp`}>
-            <button className="border border-white rounded-[48px] px-5 py-2 font-semibold border-opacity-50 ">
-              Login/Register
-            </button>
-          </Link>
-        </nav>
-        <div className="flex flex-col w-full mt-[117px] mb-10 ">
-          <h1 className="text-white text-[64px] leading-[120%] font-bold mb-10  ">
-            Discover the world on <br /> wheels with our car
-            <br /> rental service
-          </h1>
-          <p className="text-[18px] text-white leading-[160%] font-normal mb-6 ">
-            Choose from a wide range of cars that fit your style and budget.
-            <br />
-            Experience the freedom of the open road with our reliable and <br />
-            efficient rental service.
-          </p>
-          <span className=" h-[2px] max-w-[528px] bg-white opacity-[0.2] mb-[18px] "></span>
-          <div className=" flex items-center gap-x-4 ">
-            <div className=" bg-white p-[6px] rounded-3xl ">
-              <MdOutlineArrowForwardIos className=" text-black w-[18px] h-[18px]  " />
+    <div>
+      <div className=" bg-[#0F0F0F]">
+        <MainContainer className=" border text-white">
+          <nav className="flex items-center justify-between my-[22px] ">
+            <IoReorderTwoOutline className="w-5 h-5 " />
+            <Image src={HeadingLogo} alt="Heading does not show " />
+            <Link href={`/auth/signUp`}>
+              <button className="border border-white rounded-[48px] px-5 py-2 font-semibold border-opacity-50 ">
+                Login/Register
+              </button>
+            </Link>
+          </nav>
+          <div className="flex flex-col w-full mt-[117px] mb-10 ">
+            <h1 className="text-white text-[64px] leading-[120%] font-bold mb-10  ">
+              Discover the world on <br /> wheels with our car
+              <br /> rental service
+            </h1>
+            <p className="text-[18px] text-white leading-[160%] font-normal mb-6 ">
+              Choose from a wide range of cars that fit your style and budget.
+              <br />
+              Experience the freedom of the open road with our reliable and{" "}
+              <br />
+              efficient rental service.
+            </p>
+            <span className=" h-[2px] max-w-[528px] bg-white opacity-[0.2] mb-[18px] "></span>
+            <div className=" flex items-center gap-x-4 ">
+              <div className=" bg-white p-[6px] rounded-3xl ">
+                <MdOutlineArrowForwardIos className=" text-black w-[18px] h-[18px]  " />
+              </div>
+              <h3 className=" text-[18px] ">Contact us</h3>
             </div>
-            <h3 className=" text-[18px] ">Contact us</h3>
           </div>
-        </div>
-      </MainContainer>
-      <MainContainer className="bg-white ">
+        </MainContainer>
+      </div>
+      <MainContainer className=" flex items-center gap-x-4 rounded-[16px] px-6 py-[30px] opacity-90 bg-white shadow-[0_8px_8px_-4px_#101828]  ">
+        <FindVehicleInput />
         <FindVehicleInput />
       </MainContainer>
     </div>
--- src/components/inputs/FindVehicleInput.tsx
+++ src/components/inputs/FindVehicleInput.tsx
@@ -4,19 +4,19 @@ import { CiCalendar } from "react-icons/ci";
 
 const FindVehicleInput = () => {
   return (
-    <div>
-      <div>
+    <div className=" flex ">
+      <div className="  ">
         <h3 className="opacity-80 leading-[100%]  ">Pick-up Location</h3>
-        <div>
-          <IoLocationOutline />
-          <input type="text" />
+        <div className=" flex ">
+          <IoLocationOutline className="w-5 h-5 text-black  " />
+          <input type="text" placeholder="Search a Location" />
         </div>
       </div>
       <div>
-        <h3>Pick-up date</h3>
-        <div>
-          <input type="text" />
-          <CiCalendar />
+        <h3 className=" opacity-80 leading-[100%] ">Pick-up date</h3>
+        <div className=" flex">
+          <input type="text" placeholder="12/12/2023" />
+          <CiCalendar className="w-5 h-5 text-black   " />
         </div>
       </div>
     </div>
